339e306a52af1ca96060c69b03a1ee7a15a639ba
Apfelmann <Chris.Spickermann@maxouteveryday.de>
2024-02-07 19:40:14 +0100

    added movementspeed icon, Added Directory func

--- Shop/Shop.gd
+++ Shop/Shop.gd
@@ -1,12 +1,15 @@
 extends StaticBody2D
 
+var shopItems = preload("res://Shop/ShopItem/ShopItem.tscn")
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
 		%ShopOverlay.visible = true
+		shopItems.instantiate()
 
 
-func _on_area_2d_body_exited(body):
-	if body.name == "Player":
-		%ShopOverlay.visible = false
+
+#func _on_area_2d_body_exited(body):
+	#if body.name == "Player":
+		#%ShopOverlay.visible = false
 
--- Shop/ShopItem/ShopItem.gd
+++ Shop/ShopItem/ShopItem.gd
@@ -1,2 +1,16 @@
+extends Node2D
+
 func getAllAssets():
-	pass
+	var assets = []
+	var dir = DirAccess.open("res://Shop/ShopItem/assets")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name !="":
+			assets.push_back(file_name)
+			file_name = dir.get_next()
+			
+	print(assets)
+
+func _init():
+	getAllAssets()
